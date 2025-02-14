cd1623f903f866de70a7740cda6c3d3c2a2cfa4b
Huw Jones <40303012+rookie-eyes@users.noreply.github.com>
2025-02-14 13:41:38 +0000

    Create Microsoft.PowerShellISE_profile.ps1

---
+++ Microsoft.PowerShellISE_profile.ps1
@@ -0,0 +1,45 @@
+﻿### PowerShell ISE Profile Refactor
+### Version 1.03 - Refactored
+
+Set-ExecutionPolicy Bypass
+Set-Location C:\
+if ($psISE)
+{
+  Start-Steroids
+  Clear-Host
+  Write-Host 'ISE Launched' 
+  Get-Date
+  $env:USERNAME
+}
+# Initial GitHub.com connectivity check with 1 second timeout
+$canConnectToGitHub = Test-Connection github.com -Count 1 -Quiet
+
+# Import Modules and External Profiles
+
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
+# Initial GitHub.com connectivity check with 1 second timeout
+$canConnectToGitHub = Test-Connection github.com -Count 1 -Quiet
+# Network Utilities
+function Get-PubIP { (Invoke-WebRequest http://ifconfig.me/ip).Content }
+# System Utilities
+function uptime {
+    if ($PSVersionTable.PSVersion.Major -eq 5) {
+        Get-WmiObject win32_operatingsystem | Select-Object @{Name='LastBootUpTime'; Expression={$_.ConverttoDateTime($_.lastbootuptime)}} | Format-Table -HideTableHeaders
+    } else {
+        net statistics workstation | Select-String "since" | ForEach-Object { $_.ToString().Replace('Statistics since ', '') }
+    }
+}
+# Simple function to start a new elevated process. If arguments are supplied then 
+# a single command is started with admin rights; if not then a new admin instance
+# of PowerShell ISE is started.
+function ISE {
+    if ($args.Count -gt 0) {   
+        $argList = "& '" + $args + "'"
+        Start-Process "$psHome\powershell_ise.exe" -Verb runAs -ArgumentList $argList
+    } else {
+        Start-Process "$psHome\powershell_ise.exe" -Verb runAs
+    }
+}
