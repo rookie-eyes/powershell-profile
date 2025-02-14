7d81c9ff584c7e1f4d51c6a5f1c6714d1b1f05b3
Huw Jones <40303012+rookie-eyes@users.noreply.github.com>
2025-02-14 13:46:41 +0000

    Update Microsoft.PowerShellISE_profile.ps1

--- Microsoft.PowerShellISE_profile.ps1
+++ Microsoft.PowerShellISE_profile.ps1
@@ -14,6 +14,35 @@ if ($psISE)
 # Initial GitHub.com connectivity check with 1 second timeout
 $canConnectToGitHub = Test-Connection github.com -Count 1 -Quiet
 
+function Update-Profile {
+    if (-not $global:canConnectToGitHub) {
+        Write-Host "Skipping profile update check due to GitHub.com not responding within 1 second." -ForegroundColor Yellow
+        return
+    }
+
+    try {
+        $url = "https://raw.githubusercontent.com/rookie-eyes/powershell-profile/main/Microsoft.PowerShellISE_profile.ps1"
+        $oldhash = Get-FileHash $PROFILE
+        Invoke-RestMethod $url -OutFile "$env:temp/Microsoft.PowerShellISE_profile.ps1"
+        $newhash = Get-FileHash "$env:temp/Microsoft.PowerShell_profile.ps1"
+        if ($newhash.Hash -ne $oldhash.Hash) {
+            Copy-Item -Path "$env:temp/Microsoft.PowerShellISE_profile.ps1" -Destination $PROFILE -Force
+            Write-Host "Profile has been updated. Please restart your shell to reflect changes" -ForegroundColor Magenta
+        }
+    } catch {
+        Write-Error "Unable to check for `$profile updates"
+    } finally {
+        Remove-Item "$env:temp/Microsoft.PowerShellISE_profile.ps1" -ErrorAction SilentlyContinue
+    }
+}
+Update-Profile
+
+#Re-load Profile Function
+
+function reload-profile {
+    & $profile
+}
+
 # Import Modules and External Profiles
 
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
